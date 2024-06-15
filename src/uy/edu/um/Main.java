6632c557fb49c0d5137f1d446fa17e189f3413fe
FrandelaT <fdelatorre@correo.um.edu.uy>
2024-06-15 12:50:09 -0300

    corregí Main

--- src/uy/edu/um/Main.java
+++ src/uy/edu/um/Main.java
@@ -1,7 +1,8 @@
 package uy.edu.um;
 
 import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.*;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+import com.opencsv.bean.IterableCSVToBeanBuilder;
 import com.opencsv.CSVReader;
 
 
@@ -9,7 +10,10 @@ import java.io.FileReader;
 import java.io.Reader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import uy.edu.um.adt.linkedlist.MyLinkedListImpl;
 import uy.edu.um.adt.linkedlist.MyList;
 import uy.edu.um.entities.spotifyTrack;
 
@@ -18,24 +22,25 @@ public class Main {
         String csvFile = "data.csv";
 
         try (Reader reader = new FileReader(csvFile)) {
-            CsvToBean<spotifyTrack> csvToBean = new CsvToBeanBuilder<spotifyTrack>(reader)
-                    .withType(spotifyTrack.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
+            HeaderColumnNameMappingStrategy<spotifyTrack> strategy = new HeaderColumnNameMappingStrategy<>();
+            strategy.setType(spotifyTrack.class);
 
-            List<spotifyTrack> tracks = csvToBean.parse();
+            CsvToBean<spotifyTrack> csvToBean = new CsvToBean<>();
+            List<spotifyTrack> tracks = csvToBean.parse(strategy, new CSVReader(reader));
 
             // Process each track to split the "artists" field
             for (spotifyTrack track : tracks) {
                 String artists = track.getArtists();
                 if (artists != null && !artists.isEmpty()) {
                     List<String> firstartistList = Arrays.asList(artists.split(",\\s*"));
-                    MyList<String> artistList = parseFromJavaList(firstartistList);
+                    MyList<String> artistList= new MyLinkedListImpl<>();
+                    artistList = artistList.parseFromJavaList(firstartistList);
                     track.setArtistList(artistList);
                 }
                 System.out.println(track);
             }
-            MyList<spotifyTrack> Tracks = parseFromJavaList(tracks);
+            MyList<spotifyTrack> Tracks = new MyLinkedListImpl<>();
+            Tracks=Tracks.parseFromJavaList(tracks);
         } catch (Exception e) {
             e.printStackTrace();
         }
